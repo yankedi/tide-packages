9a8e667ec5e5a7d227acfcb20148144e0f0f9ed2
yan <2696596951@qq.com>
2026-04-21 15:58:14 +0000

    change Dockerfile,docker_image.yaml for tide

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 TERMUX_SCRIPTDIR=$(cd "$(realpath "$(dirname "$0")")"; cd ..; pwd)
-: ${TERMUX_BUILDER_IMAGE_NAME:=ghcr.io/termux/package-builder}
-: ${CONTAINER_NAME:=termux-package-builder}
+: ${TERMUX_BUILDER_IMAGE_NAME:=ghcr.io/yankedi/package-builder}
+: ${CONTAINER_NAME:=tide-package-builder}
 : ${TERMUX_DOCKER_RUN_EXTRA_ARGS:=}
 : ${TERMUX_DOCKER_EXEC_EXTRA_ARGS:=}
 BUILDSCRIPT_NAME=build-package.sh
